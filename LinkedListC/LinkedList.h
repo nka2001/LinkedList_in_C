6e13837aa1ffe55cdb0ec583fb9ec0df7c6d66a2
nka2001 <89935053+nka2001@users.noreply.github.com>
2023-06-24 12:19:59 -0400

    Add project files.

---
+++ LinkedListC/LinkedList.h
@@ -0,0 +1,12 @@
+
+#include <stdio.h>
+
+
+void print();
+
+void print() {
+
+	printf("Hello World");
+
+
+}
---
+++ LinkedListC/main.c
@@ -0,0 +1,16 @@
+#include "LinkedList.h"
+#include <stdio.h>
+#include <string.h>
+
+
+/*
+	this program is a simple liked list on all types, this is meant to help get me more used to C
+	programing
+*/
+
+int main(int argc, char* argv[]) {
+
+
+
+
+}
